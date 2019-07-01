bd4770878200b675bf92699993c5887bf87e60b2
valinho10 <50571449+valinho10@users.noreply.github.com>
2019-07-01 20:08:27 +0200

    Add files via upload

---
+++ lb-02/config_proxy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2 libxml2-dev
+sudo a2enmod proxy
+sudo a2enmod proxy_html
+sudo a2enmod proxy_http
+cat <<EOF | sudo tee -a /etc/apache2/sites-available/000-default.conf
+	<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        #ServerName www.example.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+		# Allgemeine Proxy Einstellungen
+    ProxyRequests Off
+    <Proxy *>
+        Order deny,allow
+        Allow from all
+    </Proxy>
+
+    # Weiterleitungen master
+    ProxyPass /web1 http://10.0.0.11
+    ProxyPassReverse /web1 http://10.0.0.11
+	
+	    # Weiterleitungen master
+    ProxyPass /web2 http://10.0.0.12
+    ProxyPassReverse /web2 http://10.0.0.12
+</VirtualHost>
+EOF
+sudo service apache2 restart
---
+++ lb-02/config_web2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2 libxml2-dev
+cat <<EOF | sudo tee -a /var/www/html/index.html
+Webseite von Server 2
+EOF
+sudo service apache2 restart
+sudo ufw allow 80/tcp
+echo "y" | sudo ufw enable
